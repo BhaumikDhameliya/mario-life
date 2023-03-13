f446e5cba7beb44a206440b89b7ea396b14b5faf
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-13 21:03:55 +0530

    create a new qwikcity project

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <div>
+      <h2>Okie Dokie</h2>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Mario Life",
+  meta: [
+    {
+      name: "description",
+      content: "a blog site about everthing mario",
+    },
+  ],
+  links: [
+    {
+      rel: "stylesheet",
+      href: "somestylesheet.com/styles.css",
+    },
+  ],
+};
