abacdf7d925be3af2d4ea768ebe484ff87524c0c
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-16 15:21:15 +0530

    Page Components and Routes

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,28 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  console.log("about page rendered....");
+  return (
+    <article>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore labore
+        cumque consectetur nemo. Ut nemo praesentium, reprehenderit nihil labore
+        maxime voluptas dolorum commodi. Placeat sit quia voluptatum dolorem
+        dolores nisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore labore
+        cumque consectetur nemo. Ut nemo praesentium, reprehenderit nihil labore
+        maxime voluptas dolorum commodi. Placeat sit quia voluptatum dolorem
+        dolores nisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore labore
+        cumque consectetur nemo. Ut nemo praesentium, reprehenderit nihil labore
+        maxime voluptas dolorum commodi. Placeat sit quia voluptatum dolorem
+        dolores nisi.
+      </p>
+    </article>
+  );
+});
