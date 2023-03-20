034e9bbb045dea1b91dbbbb43397791752103bbc
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-20 15:27:15 +0530

    Event Handlers

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,8 +1,19 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  component$,
+  useSignal,
+  useStore,
+  useStylesScoped$,
+} from "@builder.io/qwik";
 import ContactStyles from "./contact.css?inline";
 
 export default component$(() => {
   useStylesScoped$(ContactStyles);
+
+  const formVisible = useSignal(false);
+  const formState = useStore({
+    name: "",
+    message: "",
+  });
   return (
     <article>
       <h2>Contact</h2>
@@ -12,6 +23,48 @@ export default component$(() => {
         maxime voluptas dolorum commodi. Placeat sit quia voluptatum dolorem
         dolores nisi.
       </p>
+
+      <button
+        onClick$={() => {
+          formVisible.value = true;
+        }}
+      >
+        Contact Me
+      </button>
+
+      {formVisible.value && (
+        <form
+          preventdefault:submit
+          onSubmit$={() => {
+            console.log(formState.name, formState.message);
+            formState.name = "";
+            formState.message = "";
+          }}
+        >
+          <label>
+            <span>Your name:</span>
+            <input
+              type="text"
+              value={formState.name}
+              onInput$={(e) => {
+                formState.name = (e.target as HTMLInputElement).value;
+              }}
+            />
+          </label>
+          <label>
+            <span>Your message:</span>
+            <textarea
+              value={formState.message}
+              onInput$={(e) => {
+                formState.message = (e.target as HTMLInputElement).value;
+              }}
+            ></textarea>
+          </label>
+          <button>send</button>
+          <p>{formState.name}</p>
+          <p>{formState.message}</p>
+        </form>
+      )}
     </article>
   );
 });
