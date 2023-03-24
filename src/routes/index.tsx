c052ef9671a5f73ce50d577bf90c222fedbd6fe7
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-24 16:54:33 +0530

    fetching data

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,44 @@
-import { component$, useSignal, useStore } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, Resource } from "@builder.io/qwik";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
+import { useEndpoint } from "@builder.io/qwik";
+
+interface BlogData {
+  id: string;
+  title: string;
+  content: string;
+}
+
+export const onGet: RequestHandler<BlogData[]> = async () => {
+  console.log("fetching the data"); // always on the server
+
+  const res = await fetch("http://localhost:3000/blogs");
+  const data = await res.json();
+
+  return data;
+};
 
 export default component$(() => {
-  const name = useSignal("mario");
-  const person = useStore({ name: "peach", age: 30 });
-  const blogs = useStore([
-    { id: 1, title: "my first blog" },
-    { id: 2, title: "my second blog" },
-    { id: 3, title: "my third blog" },
-  ]);
+  const blogsData = useEndpoint<BlogData[]>();
+
   return (
     <div>
-      <h2>Okie Dokie</h2>
-      <p>Hello, {name.value}</p>
-      <p>
-        Hello, {person.name} - {person.age}
-      </p>
-      <button
-        onClick$={() => {
-          name.value = "luigi";
-        }}
-      >
-        click me
-      </button>
-      <button
-        onClick$={() => {
-          person.name = "bowser";
-        }}
-      >
-        click me again
-      </button>
+      <h1>Okie Dokie!</h1>
 
-      {blogs.map((blog) => {
-        return <div key={blog.id}>{blog.title}</div>;
-      })}
-
-      <button onClick$={() => blogs.pop()}>remove a blog</button>
+      <Resource
+        value={blogsData}
+        onPending={() => <div>Loading blogs...</div>}
+        onResolved={(blogs) => (
+          <div class="blogs">
+            {blogs &&
+              blogs.map((blog) => (
+                <div key={blog.id}>
+                  <h3>{blog.title}</h3>
+                  <p>{blog.content.slice(0, 50)}...</p>
+                </div>
+              ))}
+          </div>
+        )}
+      />
     </div>
   );
 });
@@ -45,13 +48,7 @@ export const head: DocumentHead = {
   meta: [
     {
       name: "description",
-      content: "a blog site about everthing mario",
-    },
-  ],
-  links: [
-    {
-      rel: "stylesheet",
-      href: "somestylesheet.com/styles.css",
+      content: "a blog site about everything Super Mario related",
     },
   ],
 };
